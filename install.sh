150d13f46e445a33cd032a53462684c46db905e5
kevin.gehling <kevin.gehling@check24.de>
2017-04-01 00:12:24 +0200

    add submodule to install

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@ curl -LSso .vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 ln -s .vim ~/.vim
 ln -s .bash_profile ~/.bash_profile
 ln -s .vimrc ~/.vimrc
+
+git submodule init
+git submodule update
