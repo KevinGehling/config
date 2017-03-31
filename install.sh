d1467150871b8591512afb41f6c468aca3050272
kevin.gehling <kevin.gehling@check24.de>
2017-04-01 00:19:45 +0200

    use correct dir for symlink

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 curl -LSso .vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-ln -s .vim ~/.vim
-ln -s .bash_profile ~/.bash_profile
-ln -s .vimrc ~/.vimrc
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ln -s $DIR/.vim ~/.vim
+ln -s $DIR/.bash_profile ~/.bash_profile
+ln -s $DIR/.vimrc ~/.vimrc
 
 git submodule init
 git submodule update
